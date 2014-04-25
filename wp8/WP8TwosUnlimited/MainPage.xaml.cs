215fe66989732a2d02b95cb1d04ca5c8851bba7d
hhtat <heinthuhtat@gmail.com>
2014-04-24 23:59:02 -0700

    Create and add logo.

--- wp8/WP8TwosUnlimited/MainPage.xaml.cs
+++ wp8/WP8TwosUnlimited/MainPage.xaml.cs
@@ -15,5 +15,10 @@ namespace WP8TwosUnlimited
         {
             this.NavigationService.Navigate(new Uri("/" + typeof(GamePage).Name + ".xaml", UriKind.Relative));
         }
+
+        private void instructionsButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Just swipe and combine!", "It's pretty simple \u2026", MessageBoxButton.OK);
+        }
     }
 }
